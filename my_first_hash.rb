7c88c0d1ebb253f4b41f0b0a78886c2d58393ab2
Judy McGoldrick <jjeanius@verizon.net>
2017-12-21 04:21:45 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,7 +19,7 @@ def retrieval
 end
 
 def adding
-  shipping_manifest["oil paintings"]
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets" => 2, "gun powder" => 4}
   # add 2 muskets to the shipping_manifest hash below
   # add 4 gun powder to the shipping_manifest hash below
   # return the shipping_manifest hash below
