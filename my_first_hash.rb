78daac2059de68abba02b0924de67f6c40f1d5ff
Judy McGoldrick <jjeanius@verizon.net>
2017-12-21 04:16:06 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,8 +19,7 @@ def retrieval
 end
 
 def adding
-
-  shipping_manifest["oil paintings"}
+  shipping_manifest["oil paintings"]
   # add 2 muskets to the shipping_manifest hash below
   # add 4 gun powder to the shipping_manifest hash below
   # return the shipping_manifest hash below
